5e06c9ec2617960c0bd79b855b0fe4e5873e4509
timharding <timharding31@gmail.com>
2020-08-27 16:42:04 -0700

    Trim video

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -10,7 +10,7 @@ export default () => {
         <p>StockFox, a completely derivative player in the world of commission-free investing, gives you very few ways to make your money work harder.</p>
         <button><Link to="/signup">Sign Up</Link></button>
     </div>
-    <video autoPlay="autoPlay" loop="loop" muted="muted" className="home-page-video" src="/assets/homepage_video-8b8fd1f90e49bc4c08deb714da9fa18acf342dd2edf5819fc1bf6cb1235b4198.mp4"></video>
+      <video autoPlay="autoPlay" loop="loop" muted="muted" className="home-page-video" src="/assets/homepage_video_2-1a0e3a448938b8b806171d54cc000f0946768ee9d307d30535ac4bdfaf04835b.mp4"></video>
     <img className="home-page-img" src="/assets/homepage_video_background-91db2d9f7fd4918147679b4c16508364c8190148d6e4d8c5b5279ff0fa4d674d.png" alt=""/>
     <div className="home-page-information">
       <div className="about-creator">
