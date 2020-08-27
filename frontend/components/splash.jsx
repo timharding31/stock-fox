9bb46db5345a27c6966269e594a74d8da29af1d1
timharding <timharding31@gmail.com>
2020-08-27 14:53:42 -0700

    Finish splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -12,6 +12,17 @@ export default () => {
     </div>
     <video autoPlay="autoPlay" loop="loop" muted="muted" className="home-page-video" src="/assets/homepage_video-8b8fd1f90e49bc4c08deb714da9fa18acf342dd2edf5819fc1bf6cb1235b4198.mp4"></video>
     <img className="home-page-img" src="/assets/homepage_video_background-91db2d9f7fd4918147679b4c16508364c8190148d6e4d8c5b5279ff0fa4d674d.png" alt=""/>
+    <div className="home-page-information">
+      <div className="about-creator">
+        <ul className="links-list">
+        <li>by <strong>Tim Harding</strong></li>
+        <li><a href="https://timharding.io" target="_blank">timharding.io</a></li>
+        <li><a href="mailto:timharding31@gmail.com" target="_blank">timharding31@gmail.com</a></li>
+        <li><a href="https://www.linkedin.com/in/timharding31/" target="_blank">LinkedIn</a></li>
+        <li><a href="https://www.github.com/timharding31" target="_blank">GitHub</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
   )
 }
